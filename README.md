c373f2251193b534a1a41c17b8db89c88753e02d
DataDeanmon <deansmeulders@yahoo.com.au>
2015-08-20 21:46:07 +1000

    Completed code and supporting files

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # UCI-HAR-Dataset
-In fulfilment of Coursera Data Science Specialisation - Getting and Cleaning Data Course Project
+
+This repo contains R scripts to produce a tidy data set from the University of California Irvine (UCI) Human Activity Recognition Using Smartphones Data Set.  The UCI dataset was built from the recordings of 30 subjects performing activities of daily living (ADL) while carrying a waist-mounted smartphone with embedded inertial sensors.  A full description is available at the site where the data was obtained:
+
+  *[http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones]
+
+## Requirements
+
+This repo has been created in fulfilment of the Coursera Data Science Specialisation - Getting and Cleaning Data Course Project.
+
+The course requirements are to create a R script that does the following:
+
+  * Merges the training and the test sets to create one data set.
+
+  * Extracts only the measurements on the mean and standard deviation for each measurement.
+  * Uses descriptive activity names to name the activities in the data set
+  * Appropriately labels the data set with descriptive variable names.
+  * From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## R Code
+
+The run_analysis.R file within this repo contains the R code to produce the tidy data set called x_Summary.txt.  The R code assumes that the source data has been downloaded from the [UCI Machine Learning Repository](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) and saved to the current working directory.  
+
+Source the R code with the following command:
+
+```R
+source("run_analysis.R")
+```
+
+The tidy data set can be loaded back into R using the command:
+
+```R
+tidy_data <- read.table("tidy_data.txt")
+```
+
+## CodeBook
+
+A CodeBook relating to the tidy data set produced by the run_analysis.R script is also available in this repo.  
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+#Load required libraries
+library(dplyr)
+
+#Read test data to workspace
+Subject_Test <- read.table("subject_test.txt")
+x_Test <- read.table("X_test.txt")
+y_Test <- read.table("y_test.txt")
+
+#Read training data to workspace
+Subject_Train <- read.table("subject_train.txt")
+x_Train <- read.table("X_train.txt")
+y_Train <- read.table("y_train.txt")
+
+#Read activity labels and features to workspace
+Activity_labels <- read.table("activity_labels.txt")
+Features <- read.table("features.txt")
+
+#Combine test and training sets
+x_Total <- rbind(x_Test, x_Train)
+
+#Rename all variables with Features data
+names(x_Total) <- Features$V2 
+
+#Combine subjects and activities to total (test and training) datasets
+names(Subject_Test) <- "Subject" #rename column name in Subject_Test
+names(Subject_Train) <- "Subject" #rename column name in Subject_Train
+x_Total <- cbind(x_Total, rbind(Subject_Test, Subject_Train)) #Add subjects to x_Total as new column
+names(y_Test) <- "ActivityNo" #rename column name in y_Test
+names(y_Train) <- "ActivityNo" #rename column name in y_Train
+x_Total <- cbind(x_Total, rbind(y_Test, y_Train)) #Add activities to x_Total as new column
+x_Total <- merge(x_Total, Activity_labels, by.x = "ActivityNo", by.y = "V1") #add  descriptive activity names in x_Total
+
+#Extract measurements relating to mean and standard deviation 
+measures <- grep("mean()", names(x_Total)) #finds all measures with "mean()" in column name
+measures <- c(measures, grep("std()", names(x_Total))) #finds all measures with "std()" in column name
+remove <- grep("meanFreq", names(x_Total)) #finds all measures with "meanFreq" in column name
+measures <- measures[! measures %in% remove] #removes measures with "meanFreq" in name
+measures <- sort(measures) #sorts measures in ascending order
+measures <- c(measures, 563, 564) #adds subject and activitylabel columns to measures to keep in final extract 
+x_Extract <- x_Total[, measures] #creates x_Extract, selecting only "mean" and "std" variables and subject and ActivityLabel
+x_Extract <- rename(x_Extract, ActivityLabel = V2) #rename V2 to "ActivityLabel
+
+# make feature names more human readble
+names(x_Extract) <- gsub("Acc", "-Acceleration", names(x_Extract))
+names(x_Extract) <- gsub("Mag", "-Magnitude", names(x_Extract))
+
+#Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+x_Summary <- x_Extract %>% 
+        group_by(ActivityLabel, Subject) %>%
+        summarise_each(funs(mean))
+write.table(x_Summary, file = "x_Summary.txt", row.names = FALSE)
